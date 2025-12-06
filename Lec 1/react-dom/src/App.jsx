aa8b1c4e93759c28ceb64f1b3ba13359dc7873a3
akshay pawar <ap3690700@gmail.com>
2025-12-07 01:17:58 +0530

    Learned about props in react and destructuring of props and solved assignment for that

--- Lec 1/react-dom/src/App.jsx
+++ Lec 1/react-dom/src/App.jsx
@@ -6,13 +6,15 @@ import Greeting from './components/Greeting'
 import ProductInfo from './components/ProductInfo'
 import UserList from './components/UserList'
 import ProductList from './components/ProductList'
+import Person from './components/Person'
+import Product from './components/Product'
 
 
 const App = () => {
   return (
-    <div className='flex justify-around'>
-    <UserList/>
-    <ProductList/>
+    <div className='flex justify-evenly'>
+      <Person name="akshay" age={22}/>
+      <Product name="Lenovo monitor" price={20000}/>
     </div>
   )
 }
---
+++ Lec 1/react-dom/src/components/Person.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Person = ({name,age}) => { //destructuring the props obj
+  return (
+    <div>
+        <h2>{name}</h2>
+        <p>{age}</p>
+    </div>
+  )
+}
+
+export default Person
